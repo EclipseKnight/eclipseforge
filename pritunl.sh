172f0bcad86a8a81bac99ac920724f16086db5de
EclipseKnight <32472185+EclipseKnight@users.noreply.github.com>
2025-09-04 10:57:53 -0400

    Create pritunl.sh

---
+++ pritunl.sh
@@ -0,0 +1,16 @@
+sudo tee /etc/apt/sources.list.d/openvpn.list << EOF
+deb [ signed-by=/usr/share/keyrings/openvpn-repo.gpg ] https://build.openvpn.net/debian/openvpn/stable noble main
+EOF
+
+sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+deb [ signed-by=/usr/share/keyrings/pritunl.gpg ] https://repo.pritunl.com/unstable/apt noble main
+EOF
+
+sudo apt --assume-yes install gnupg
+curl -fsSL https://swupdate.openvpn.net/repos/repo-public.gpg | sudo gpg -o /usr/share/keyrings/openvpn-repo.gpg --dearmor --yes
+curl -fsSL https://raw.githubusercontent.com/pritunl/pgp/master/pritunl_repo_pub.asc | sudo gpg -o /usr/share/keyrings/pritunl.gpg --dearmor --yes
+
+sudo apt update
+sudo apt --assume-yes install pritunl wireguard-tools
+sudo systemctl start pritunl
+sudo systemctl enable pritunl
